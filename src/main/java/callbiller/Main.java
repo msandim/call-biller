c953c610cfa750446e509c93f81f1fe5b9de4ec4
msandim <migsandim@hotmail.com>
2018-06-26 05:00:56 +0100

    indentation

--- src/main/java/callbiller/Main.java
+++ src/main/java/callbiller/Main.java
@@ -26,16 +26,16 @@ public class Main {
 
         try (Stream<String> stream = Files.lines(Paths.get(filename))) {
 
-			stream.forEach(line -> {
+            stream.forEach(line -> {
                 try {
                     CallRecord record = CallRecord.parse(line); // parse the line as a call record
                     biller.addCall(record); // add call record to the biller:
-				} catch (Exception e) {
-					System.err.println("Warning: the following line is an invalid call record: " + line);
+                } catch (Exception e) {
+                    System.err.println("Warning: the following line is an invalid call record: " + line);
                 }
             });
 
-		} catch (IOException e) {
+        } catch (IOException e) {
             System.err.println("Error: the file \"" + filename + "\" could not be found.");
             return new BigDecimal(-1);
         }
